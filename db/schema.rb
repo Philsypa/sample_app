25e6204fa60ddff1a893e0d5fa397dd3a8e544fe
Phil Glatts <Philsypa@gmail.com>
2016-05-22 23:38:55 -0400

    added price col to product DB

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160520025634) do
+ActiveRecord::Schema.define(version: 20160523024650) do
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 20160520025634) do
     t.string   "image_url"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "colour"
+    t.decimal  "price"
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Product.create(:name => "Small Biz Website", :description => "Websites targeted specifically to small businesses!", :image_url => "website3.jpg", :colour =>"", :price =>1500.00 )
+Product.create(:name => "Small Biz Store", :description => "E-Commerce driven websites targeted specifically to small businesses", :image_url =>"e_com5.jpg", :colour =>"", :price =>2500.00 )
+Product.create(:name => "Website Redesign", :description => "Unhappy with your website, let us redesign it for you!", :image_url =>"html.jpeg", :colour =>"", :price =>1000.00 )
+Product.create(:name => "Enhanced Web functionality", :description => "Looking to use new enhanced technology in your website? Talk to us about it!", :image_url =>"ruby2.jpg", :colour =>"", :price =>2000.00 )
